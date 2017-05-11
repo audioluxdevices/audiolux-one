dabedda66fd30c374b01c3c4c292fe6bcb02eec3
CymaSpace <mylesdebastion@Myless-iMac.local>
2017-05-11 12:49:25 -0700

    cleaned up code for Eric's sketch

--- experiments/neopixel-simple-rgbw-pot-controls/neopixel-simple-rgbw-pot-controls.ino
+++ experiments/neopixel-simple-rgbw-pot-controls/neopixel-simple-rgbw-pot-controls.ino
@@ -6,19 +6,16 @@
   #include <avr/power.h>
 #endif
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
+// Define which pin on the Audiolux is connected to the LED Data Input
 #define PIN            6
 
-// How many NeoPixels are attached to the Arduino?
+// How many LEDs are attached to the Audiolux?
 #define NUMPIXELS      33
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
 // example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_RWGB + NEO_KHZ800);
-
-int delayval = 500; // delay for half a second
 
 /* 
   dim_curve 'lookup table' to compensate for the nonlinearity of human vision.
@@ -44,10 +41,7 @@ const byte dim_curve[] = {
     146, 149, 151, 154, 157, 159, 162, 165, 168, 171, 174, 177, 180, 183, 186, 190,
     193, 196, 200, 203, 207, 211, 214, 218, 222, 226, 230, 234, 238, 242, 248, 255,
 };
-//const int sensorPin = 2;  // pin the potmeter is attached too
-//int sensorVal = 0; // store the value coming from the sensor
-//int fadeVal   = 0; // value that changes between 0-255
-//int fadeSpeed = 4; // 'speed' of fading
+
 int rgb_colors[3]; 
  
 int hue;
@@ -56,59 +50,27 @@ int brightness;
 int white;
 
 void setup() {
-  Serial.begin(9600);      // open the serial port at 9600 bps: 
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-#if defined (__AVR_ATtiny85__)
-  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-#endif
-  // End of trinket special code
-
   pixels.begin(); // This initializes the NeoPixel library.
 }
 
 void loop() {
-  //sensorVal = analogRead(sensorPin);     
-  //Serial.println(sensorVal);
-  // fade from 0 - 255 and back with a certain speed   
- // fadeVal = fadeVal + fadeSpeed;         // change fadeVal by speed
-  //fadeVal = constrain(fadeVal, 0, 255);  // keep fadeVal between 0 and 255
- 
-  //if(fadeVal==255 || fadeVal==0)         // change from up>down or down-up (negative/positive)
-  //{ fadeSpeed = -fadeSpeed;  
-  //}
-
+  // Assign potentiometer knobs to variables
   int potControlWhiteness = analogRead(2); // assign analog reading of left potentiometer knob to control Whiteness
   int potControlHue = analogRead(3);  // assign analog reading of right potentiometer knob to control hue
 
-  int brightness = 255;
+  // set HSB & White values
   int hue = map(potControlHue, 0, 1023, 0, 255); // map right pot knob to 0-255 values of hue
+  int saturation = 255;                               // saturation is a number between 0 - 255
+  int brightness = 255;
   int whiteness = map(potControlWhiteness, 1023, 0, 0, 255); // map left pot knob to 0-255 values of Whiteness
 
-
-  // set HSB values
-  //hue        = map(sensorVal,0, 1023,0, 359);     // hue is a number between 0 and 360
-  saturation = 255;                               // saturation is a number between 0 - 255
-  //brightness = fadeVal;                           // value is a number between 0 - 255
- 
   getRGB(hue,saturation,brightness,rgb_colors);   // converts HSB to RGB
  
- // analogWrite(ledPinR, rgb_colors[0]);            // red value in index 0 of rgb_colors array
- // analogWrite(ledPinG, rgb_colors[1]);            // green value in index 1 of rgb_colors array
- // analogWrite(ledPinB, rgb_colors[2]);            // blue value in index 2 of rgb_colors array
- 
-  delay(20); // delay to slow down fading
-  
   // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-
   for(int i=0;i<NUMPIXELS;i++){
-
     // pixels.Color takes RWGB values, from 0,0,0,0 up to 255,255,255,255
-    //pixels.setPixelColor(i, pixels.Color(0,255,0,0)); // Moderately bright blue color.
     pixels.setPixelColor(i, pixels.Color(rgb_colors[0],whiteness,rgb_colors[1],rgb_colors[2])); // Color order: R,W,G,B.
-
     pixels.show(); // This sends the updated pixel color to the hardware.
-
-    //delay(delayval); // Delay for a period of time (in milliseconds).
 
   }
 }
